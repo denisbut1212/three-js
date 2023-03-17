ef614d88589d6b15b2747c03d7903e93f2cb590d
denisbut1212 <butd@omegafuture.ru>
2023-03-17 18:51:22 +0300

    import redfab model, play its animations

--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,67 @@
-import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import * as THREE from 'three';
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
 
-const scene = new THREE.Scene()
+const pathToModel = 'assets/models/Red_Fab_zapek.glb';
+
+const scene = new THREE.Scene();
+const stats = Stats();
+const renderer = new THREE.WebGLRenderer();
 const clock = new THREE.Clock();
-
-const light = new THREE.PointLight()
-light.position.set(0.8, 1.4, 1.0)
-scene.add(light)
-
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight)
-
+const gltfLoader = new GLTFLoader();
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     10000
-)
-camera.position.set(5.0, 5.0, 5.0)
-
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
+);
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
 
-let mixer;
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-    'assets/models/bot.fbx',
-    (object) => {
-        scene.add(object);
-        mixer = new THREE.AnimationMixer(object);
-        const anim =  mixer.clipAction(object.animations[0]);
-        anim.play();
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+document.body.appendChild(stats.dom);
 
-window.addEventListener('resize', onWindowResize, false)
+
+controls.enableDamping = true;
+controls.target.set(1, 0, 0);
+
+camera.position.set(0.5, 5, 5);
+camera.rotation.set(-0.5, 0, 0);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let file = await gltfLoader.loadAsync(pathToModel);
+let model = file.scene;
+model.position.set(0, 0, 0)
+model.rotation.set(0, 60, 0)
+
+scene.add(model)
+
+let mixer = new THREE.AnimationMixer(model);
+
+file.animations.forEach((clip) => {
+    let anim = mixer.clipAction(clip);
+    anim.play();
+})
+
+window.addEventListener('resize', onWindowResize, false);
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
 }
-
-const stats = Stats()
-document.body.appendChild(stats.dom)
 
 function animate() {
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
+    controls.update();
+    stats.update();
+    mixer.update(clock.getDelta())
 
-    mixer.update(clock.getDelta());
-    controls.update()
-
-    render()
-
-    stats.update()
+    let deltaTime = clock.getDelta();
+    mixer.update(deltaTime);
+    renderer.render(scene, camera);
 }
 
-function render() {
-    renderer.render(scene, camera)
-}
-
-animate()
+animate();
